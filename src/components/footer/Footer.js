f1ba43f05a7f12933b34549ed285cd4f653b5f55
Aakash Patel <patelaakash150@gmail.com>
2026-02-28 19:57:09 -0700

    Clean up Footer component by removing unused code
    Removed unused greeting import and commented-out ToggleSwitch component.

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,7 @@
 import React from "react";
 import "./Footer.css";
 import { Fade } from "react-reveal";
-import { greeting } from "../../portfolio.js";
 /* eslint-disable jsx-a11y/accessible-emoji */
-
 export default function Footer(props) {
   return (
     <div className="footer-div">
@@ -11,7 +9,6 @@ export default function Footer(props) {
         <p className="footer-text" style={{ color: props.theme.secondaryText }}>
           Made with <span role="img">❤️</span> by Aakash Patel
         </p>
-        {/* <ToggleSwitch theme={props.theme} onToggle={props.onToggle}/> */}
       </Fade>
     </div>
   );
